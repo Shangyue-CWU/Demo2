602b3ffc9ae61e8ef56bfc527548bcabf924e6b8
Shangyue-CWU <zhush@cwu.edu>
2023-10-02 10:09:59 -0700

    Added Cat funtion on Cat.java

--- Cat.java
+++ Cat.java
@@ -5,6 +5,15 @@ public class TestSuite {
 
     }
 
-	
+}
+
+public class Cat{
+    private String name;
+    private int age;
+
+    public Cat(String name, int age){
+        this.age = age;
+        this.name = name;
+    }
 }
 
